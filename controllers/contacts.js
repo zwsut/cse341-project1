de6bec2e9223bc09f28e6e1012a08924d3bba20e
zwsutherland <zwsutherland@gmail.com>
2025-09-20 12:25:22 -0400

    week3 stuff

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,23 +2,48 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    //#swagger.tags=['Contacts]
-    const result = await mongodb.getDatabase().db().collection('contacts').find();
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
-}
+  //#swagger.tags=['Contacts']
+  try {
+    const contacts = await mongodb
+      .getDatabase()
+      .db()
+      .collection('contacts')
+      .find()
+      .toArray();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['Contacts]
+  //#swagger.tags=['Contacts']
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a contact.');
+  };
+  try {
     const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId });
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    });
-}
+
+    const result = await mongodb
+      .getDatabase()
+      .db()
+      .collection('contacts')
+      .find({ _id: contactId })
+      .toArray();
+
+    if (!result || result.length === 0) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result[0]);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
 // Week 2 additions
 const createContact = async (req, res) => {
     //#swagger.tags=['Contacts]
@@ -33,12 +58,15 @@ const createContact = async (req, res) => {
     if (response.acknowledged) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || "Some error occurred while updating the contact.");
+        res.status(500).json(response.error || "Some error occurred while creating the contact.");
     }
 };
 
 const updateContact = async (req, res) => {
     //#swagger.tags=['Contacts]
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to update a contact.');
+  };
     const contactId = new ObjectId(req.params.id);
     const contact = {
         firstName: req.body.firstName,
@@ -57,6 +85,9 @@ const updateContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
     //#swagger.tags=['Contacts]
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to delete a contact.');
+  }
     try {
         const id = req.params.id;
         const result = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: new ObjectId(id) });
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
+const validation = require('../middleware/validate');
 
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
 // Week 2 additions
-router.post('/', contactsController.createContact);
-router.put('/:id', contactsController.updateContact);
+router.post('/', validation.saveContact, contactsController.createContact);
+router.put('/:id', validation.saveContact, contactsController.updateContact);
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
